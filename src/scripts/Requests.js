eddf77641277084c0c142b94e3aa64801e2ff1dd
Jennifer-Blue-Williams <jennifer.blue.williams@gmail.com>
2022-01-02 23:01:38 -0600

    sinkRepairComplete

--- src/scripts/Requests.js
+++ src/scripts/Requests.js
@@ -1,15 +1,40 @@
 import { getRequests } from "./dataAccess.js"
+import { deleteRequest } from "./dataAccess.js"
+
 
 export const Requests = () => {
-    const requests = getRequests()
+    const requestsArray = getRequests()
+    const requestHtml = (request) => {
+        // build entire line of list item
+        return `<li> ${request.address} ${request.description} ${request.budget}${request.date}
+        
+         
+        <button class="request__delete"
+                id="request--${request.id}">
+            Delete
+        </button>
+        </li>`
+
+    
+    }
 
     let html = `
         <ul>
             ${
-                requests.map((request) => ({...request}))
-            }
-        </ul>
-    `
-
+            requestsArray.map((request) => requestHtml(request))}
+    </ul>`
+    
     return html
 }
+   
+
+
+
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [,requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
